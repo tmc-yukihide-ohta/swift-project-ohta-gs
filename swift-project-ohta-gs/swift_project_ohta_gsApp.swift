beb4e7fe8432c83c7e6f2541dc3711da33cc0ba9
tmc-yukihide-ohta <yukihide_ohta@mail.toyota.co.jp>
2023-06-12 13:44:36 +0900

    Initial Commit

---
+++ swift-project-ohta-gs/swift_project_ohta_gsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_project_ohta_gsApp.swift
+//  swift-project-ohta-gs
+//
+//  Created by user on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct swift_project_ohta_gsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-project-ohta-gsUITests/swift_project_ohta_gsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift_project_ohta_gsUITestsLaunchTests.swift
+//  swift-project-ohta-gsUITests
+//
+//  Created by user on 2023/06/12.
+//
+
+import XCTest
+
+final class swift_project_ohta_gsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
